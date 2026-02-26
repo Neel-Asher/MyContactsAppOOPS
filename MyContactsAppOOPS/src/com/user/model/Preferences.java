817af656bd22dc71742c5952621f4482f2baa2a2
Neel-Asher <neel.asher20@gmail.com>
2026-02-26 20:50:47 +0530

    Coded to enable Profile Management for user in UC3

---
+++ MyContactsAppOOPS/src/com/user/model/Preferences.java
@@ -0,0 +1,37 @@
+package com.user.model;
+
+// preferences class for customized user experience
+import com.user.exception.InvalidUserDataException;
+public class Preferences {
+
+    private boolean darkMode;
+    private boolean emailNotifications;
+
+    public Preferences() {
+        this.darkMode = false;
+        this.emailNotifications = true;
+    }
+
+    public boolean isDarkMode() {
+        return darkMode;
+    }
+
+    public boolean isEmailNotifications() {
+        return emailNotifications;
+    }
+
+    public void setDarkMode(boolean darkMode) {
+        this.darkMode = darkMode;
+    }
+
+    public void setEmailNotifications(boolean emailNotifications) {
+        this.emailNotifications = emailNotifications;
+    }
+
+    public void updatePreferences(boolean darkMode, boolean emailNotifications)
+            throws InvalidUserDataException {
+
+        this.darkMode = darkMode;
+        this.emailNotifications = emailNotifications;
+    }
+}
